aeef3db6d797fcbd1d7bdd4e765a8fa31ffdc784
Andres <andres.rovira@gmail.com>
2024-06-05 21:57:33 -0600

    Update parameter to specify config path

--- .github/workflows/scripts/generateConfigFile.sh
+++ .github/workflows/scripts/generateConfigFile.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-echo "export const config = {" > ../conf/sdConfig.ts
-echo "    models: [" >> ../conf/sdConfig.ts
-echo "        {" >> ../conf/sdConfig.ts
-echo "            id: \"1\"," >> ../conf/sdConfig.ts
-echo "            name: \"Beam Model\"," >> ../conf/sdConfig.ts
-echo "            beamId: '${BEAM_APP_ID}'" >> ../conf/sdConfig.ts
-echo "        }" >> ../conf/sdConfig.ts
-echo "    ]," >> ../conf/sdConfig.ts
-echo "    beam: {" >> ../conf/sdConfig.ts
-echo "        authToken: '${BEAM_AUTH_TOKEN}'" >> ../conf/sdConfig.ts
-echo "    }" >> ../conf/sdConfig.ts
-echo "};" >> ../conf/sdConfig.ts
+if [ -z "$1" ]; then
+    echo "Usage: $0 <output_file>"
+    exit 1
+fi
+
+OUTPUT_FILE=$1
+
+echo "export const config = {" > $OUTPUT_FILE
+echo "    models: [" >> $OUTPUT_FILE
+echo "        {" >> $OUTPUT_FILE
+echo "            id: \"1\"," >> $OUTPUT_FILE
+echo "            name: \"Beam Model\"," >> $OUTPUT_FILE
+echo "            beamId: '${BEAM_APP_ID}'" >> $OUTPUT_FILE
+echo "        }" >> $OUTPUT_FILE
+echo "    ]," >> $OUTPUT_FILE
+echo "    beam: {" >> $OUTPUT_FILE
+echo "        authToken: '${BEAM_AUTH_TOKEN}'" >> $OUTPUT_FILE
+echo "    }" >> $OUTPUT_FILE
+echo "};" >> $OUTPUT_FILE
