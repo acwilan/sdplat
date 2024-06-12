c39814f4cd789ceabe7bf3e34f4f16499a2491c9
Andres Rov <andres.rovira@gmail.com>
2024-06-11 19:00:17 -0600

    feat: Reinvent top and left menu (#30)
    * [29-reinvent-top-and-left-menu] Reinvent navigation
    
    * [29-reinvent-top-and-left-menu] Fix tests
    
    * [29-reinvent-top-and-left-menu] Update generate config file script with new structure and options

--- ui/src/components/Navigation.tsx
+++ ui/src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavbarText } from 'react-bootstrap';
 import './Navigation.css';
 
 const Navigation: React.FC = () => {
@@ -8,9 +8,9 @@ const Navigation: React.FC = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/txt2img">Text to image</Nav.Link>
-          <Nav.Link as={NavLink} to="/img2img">Image to image</Nav.Link>
-          <Nav.Link as={NavLink} to="/txt2vid">Text to video</Nav.Link>
+          <Nav.Link as={NavLink} to="/beam">Beam</Nav.Link>
+          <Nav.Link as={NavbarText}>Modelslab</Nav.Link>
+          <Nav.Link as={NavbarText}>Stability AI</Nav.Link>
         </Nav>
       </Navbar.Collapse>
   );
--- ui/src/components/Txt2Img.tsx
+++ ui/src/components/Txt2Img.tsx
@@ -3,15 +3,20 @@ import { FormComponent, FormData, ModelInfo } from "./FormComponent";
 import { BeamClient, Txt2ImgRequest } from "../api";
 import { config } from '../../../conf/sdConfig';
 import OutputView from "./OutputView";
+import { PathComponentProps } from "../types";
 
-const sortedModels: ModelInfo[] = config.models.sort((a: ModelInfo, b: ModelInfo) => a.name.localeCompare(b.name));
+const { authToken, models } = config.beam;
+const sortedModels: ModelInfo[] = Object.entries(models)
+    .map(([id, model]) => ({
+        id,
+        name: model.name
+    })).sort((a: ModelInfo, b: ModelInfo) => a.name.localeCompare(b.name));
 const modelMap: { [id: string ]: ModelInfo} = sortedModels.reduce((map, model) => {
     map[model.id] = model;
     return map;
 }, {} as { [id: string]: ModelInfo });
-const authToken: string = config.beam.authToken;
 
-const Txt2Img: React.FC = () => {
+const Txt2Img: React.FC<PathComponentProps> = ({ pathSegment }) => {
     const [ imgUrl, setImgUrl ] = useState<string>('');
     const [ outputMessage, setOutputMessage ] = useState<string>('');
     const [ messageType, setMessageType ] = useState<'success' | 'danger' | 'info'>();
